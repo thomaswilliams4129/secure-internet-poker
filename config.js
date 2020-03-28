8edbf4e911806887d16ecfeb884c3c5ccad7df9c
Noah Baldwin <nbaldwin3689@flpoly.org>
2020-03-28 11:06:50 -0400

    Update db connection

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ const { Pool } = require('pg');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+let connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+connectionString = `postgresql://pbeisgzm:vP4migqUdJULP8w_dE2dYJK-FwYpJBBx@rajje.db.elephantsql.com:5432/pbeisgzm`;
 
 const pool = new Pool({
     connectionString,
