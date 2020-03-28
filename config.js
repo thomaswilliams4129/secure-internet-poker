f54b9c959e9902616045f79dc55f093ccd904880
Noah Baldwin <nbaldwin3689@flpoly.org>
2020-03-28 11:16:15 -0400

    Update env variables

--- config.js
+++ config.js
@@ -4,9 +4,7 @@ const { Pool } = require('pg');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-let connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
-connectionString = `postgresql://pbeisgzm:vP4migqUdJULP8w_dE2dYJK-FwYpJBBx@rajje.db.elephantsql.com:5432/pbeisgzm`;
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
     connectionString,
