5c625dc1e7451d47d91c8328f316b0ee6c4e7d31
Noah Baldwin <nbaldwin3689@flpoly.org>
2020-03-28 11:04:43 -0400

    Heroku update

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString,
     ssl: false,
     connectionTimeoutMills: 100,
 });
