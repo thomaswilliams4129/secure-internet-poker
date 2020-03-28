cff654ede4c390302a02975b8314d49b25ab841e
Noah Baldwin <nbaldwin3689@flpoly.org>
2020-03-28 10:46:51 -0400

    Update for heroku

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 });
 
 // Start server
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log('Listening on localhost:3000');
 });
 const io = require('socket.io')(server);
